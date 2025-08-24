890f94bfbbf76224611adef4cab719dd2eab0432
krupa-aithal <krupaaithal82@gmail.com>
2025-08-24 15:06:48 +0530

    Add files via upload

---
+++ Showroom.java
@@ -0,0 +1,23 @@
+package car;
+
+public class Showroom 
+{
+	public static void main(String[] args) {
+		Car c1=new Car("Toyota","Corolla",15000,18.5);
+		Car c2=new Car("Honda","Civic",20000,16.0);
+		Car c3=new Car("Hyundai","i20",12000,20.0);
+		
+		c1.displayDetails();
+		c2.displayDetails();
+		c3.displayDetails();
+		c1.setOwnerName("krupa");
+		c1.updatePrice(16000);
+		System.out.println("after updates");
+		c1.displayDetails();
+		Car.showShowroomName();
+		Car.showTotalCars();
+		
+	}
+}
+
+	
